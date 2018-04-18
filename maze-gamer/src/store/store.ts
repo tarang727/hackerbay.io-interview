0a0c9c34bf99f499c692a96d46507d2d87de9d72
John Waweru Wambugu <wambugu.john.waweru@outlook.com>
2018-04-18 17:55:16 +0300

    added redux devtools

--- maze-gamer/src/store/store.ts
+++ maze-gamer/src/store/store.ts
@@ -11,5 +11,6 @@ export interface AppState {
 export const store = createStore<AppState, any, any, any>(
   combineReducers({
     game: gameReducer
-  })
+  }),
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 );
